d45d8124da372493df87decab24b62044cb08a2b
Parth Pandya <34797335+parthpandyappp@users.noreply.github.com>
2021-05-04 15:03:16 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -100,12 +100,12 @@ export default function Main(props) {
                         return (<div className="todo-item">
                             <input id="strike" type="checkbox" /><p for="strike" class="strikethrough" style={{ color: "black" }} key={item.id}>{item.todo} </p> <BiTrash style={{ marginLeft: "auto", cursor: "pointer" }} onClick={() => removeItem(item.id)} className="icon" />
                         </div>)
-                    }) : <h4>Add items to the docket to create your personalised todo-list.<br/>[Nav -> Add item]</h4>}
+                    }) : <h4>Add items to the docket to create your personalised todo-list.<br/><br/>[Nav -> Add item]</h4>}
 
                 </div>
                 :
                 <div className="todo-list box">
-                    <h4>User needs to authenticate for the application prior to using it.<br/>[Nav -> Login]</h4>
+                    <h4>User needs to authenticate for the application prior to using it.<br/><br/>[Nav -> Login]</h4>
                 </div>}
 
         </div>
